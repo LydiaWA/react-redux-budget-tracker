faf90b93207a8e0c7b06d9e44cd9d732362ed4b0
LydiaWA <lydialston@gmail.com>
2018-11-26 14:35:22 -0800

    create expenseAction.js and incomeActions.js with functions

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux';
 import rootReducer from './rootReducer';
 
-const rootStore = createStore(
+const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default rootStore;
+export default store;
